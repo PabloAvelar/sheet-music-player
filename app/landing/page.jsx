5b6288fe6da445180835175ca18b31e0e1a2ec40
dieguitobonito <diego.dominguez0356@alumnos.udg.mx>
2024-11-04 17:35:36 -0600

    Add icon
    Ni se puede agregar

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,16 +1,16 @@
-import { Button } from '@nextui-org/react';
+import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, cn} from '@nextui-org/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 
-export default function LandingPage() { // Fixed typo from LandinPage
+export default function LandingPage() {
   return (
-    <div class="h-screen w-screen bg-nord-6">
-      <div class="flex items-center justify-center h-full p-12">
-        <div class="bg-nord-15 p-12 rounded-lg">
-          <h1 class="text-9xl text-nord-0">Eyelody</h1>
+    <div className="h-screen w-screen bg-nord-6">
+      <div className="flex flex-col items-center justify-center h-full p-12">
+        <div className="bg-nord-15 p-12 rounded-lg">
+          <h1 className="text-9xl text-nord-0">Eyelody</h1>
         </div>
-        <div class="bg-nord-1">
-          <Button>Hola</Button>
-        </div>
+        <Button startContent={<FontAwesomeIcon icon={faUpload} className="text-nord-0"/>} className="bg-nord-7 mt-12">Upload</Button>
       </div>
-    </div> 
+    </div>
   );
 }
