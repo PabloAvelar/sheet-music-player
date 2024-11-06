be85e8c566e84add82e7ba2c701334ee7c884104
dieguitobonito <diego.dominguez0356@alumnos.udg.mx>
2024-11-06 16:18:33 -0600

    Add profile picture
    Se ve rara esa api, nomás para placeholder

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,15 +1,38 @@
-import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, cn} from '@nextui-org/react';
+import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from '@nextui-org/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
 
 export default function LandingPage() {
   return (
     <div className="h-screen w-screen bg-nord-6">
-      <div className="flex flex-col items-center justify-center h-full p-12">
+      {/* User icon */}
+      <div className="w-full p-4 flex justify-end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar 
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="primary"
+              size="lg"
+              src="https://i.pravatar.cc/150"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions">
+            <DropdownItem key="profile">Profile</DropdownItem>
+            <DropdownItem key="settings">Settings</DropdownItem>
+            <DropdownItem key="logout" className="text-danger" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+
+      <div className="flex flex-col items-center justify-center h-[calc(100%-4rem)] p-12">
         <div className="bg-nord-15 p-12 rounded-lg">
           <h1 className="text-9xl text-nord-0">Eyelody</h1>
         </div>
-        <Button startContent={<FontAwesomeIcon icon={faUpload} className="text-nord-0"/>} className="bg-nord-7 mt-12">Upload</Button>
+        <Button className="bg-nord-7 mt-12">Upload</Button>
       </div>
     </div>
   );
