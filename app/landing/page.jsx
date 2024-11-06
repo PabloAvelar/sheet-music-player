6f09a313aa0fede789798c64b1e2d32b3b105628
dieguitobonito <diego.dominguez0356@alumnos.udg.mx>
2024-11-06 16:51:16 -0600

    Add upload functionality
    No del todo, falta que guarde en servidor y así

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,11 +1,29 @@
+"use client";
+
 import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from '@nextui-org/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { useState, useRef } from 'react';
 
 export default function LandingPage() {
+  const fileInputRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      console.log('File selected:', file.name);
+    }
+  };
+
   return (
     <div className="h-screen w-screen bg-nord-6">
-      {/* User icon */}
+      {/* Navigation bar */}
       <div className="w-full p-4 flex justify-end">
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
@@ -28,11 +46,20 @@ export default function LandingPage() {
         </Dropdown>
       </div>
 
+      {/* Existing content */}
       <div className="flex flex-col items-center justify-center h-[calc(100%-4rem)] p-12">
         <div className="bg-nord-15 p-12 rounded-lg">
           <h1 className="text-9xl text-nord-0">Eyelody</h1>
         </div>
-        <Button className="bg-nord-7 mt-12">Upload</Button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className="hidden"
+          accept=".png,.jpg,.jpeg"
+        />
+        <Button className="bg-nord-7 mt-12"
+        onClick={handleFileClick}>Upload</Button>
       </div>
     </div>
   );
